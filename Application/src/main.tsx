0505f7835ef652cfeaff0a7c86329bc50655b3b7
Kshitij Kotecha <kotechakshitij@gmail.com>
2025-02-24 11:59:57 +0530

    MAde dark Mode Default

--- Application/src/main.tsx
+++ Application/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Set dark mode by default
+if (localStorage.getItem("theme") !== "light") {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
